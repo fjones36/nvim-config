2d5f6a7979ad829fa293a495650a50091e2cda6f
Frank Jones <fjones@gmail.com>
2024-02-23 22:19:46 -0500

    Updated install with CoC.

--- lua/fjones/packer.lua
+++ lua/fjones/packer.lua
@@ -322,4 +322,9 @@ return require('packer').startup(function(use)
     -- use("github/copilot.vim")
     -- use("eandrju/cellular-automaton.nvim")
     -- use("laytan/cloak.nvim")
+    use { 'neoclide/coc.nvim', branch='release' }
+
+    use({
+      "stevearc/conform.nvim",
+    })
 end)
